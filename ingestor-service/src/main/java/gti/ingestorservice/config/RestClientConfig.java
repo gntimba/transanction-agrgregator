cb866e173c068dc4ec2dec21915cdc1efef3b8dc
Godfrey <gntimba@gmail.com>
2026-06-06 23:57:21 +0200

    everything is working

--- ingestor-service/src/main/java/gti/ingestorservice/config/RestClientConfig.java
+++ ingestor-service/src/main/java/gti/ingestorservice/config/RestClientConfig.java
@@ -1,16 +1,19 @@
 package gti.ingestorservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
+    @Value("${spring.aggregator.base-url}")
+    private String aggregatorBaseUrl;
 
     @Bean
     public RestClient restClient() {
         return RestClient.builder()
-                .baseUrl("http://localhost:8082")
+                .baseUrl(aggregatorBaseUrl)
                 .build();
     }
 }
